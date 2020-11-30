1f8186e1c019bf49a31b395ceba1f4958e617111
whario <mariocepedaortega@gmail.com>
2020-11-30 17:00:29 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,35 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import "../../styles/index.scss";
 
 //create your first component
 export function Home() {
+	const [state, setState] = useState({
+		red: "active",
+		yellow: "noactive",
+		green: "noactive" //pongo cualquier palabra que no esté definida para que no me pinte nada
+	});
+	const handelRed = () => {
+		setState({ red: "active", yellow: "noactive", green: "noactive" });
+	};
+	const handelYellow = () => {
+		setState({ red: "noactive", yellow: "active", green: "noactive" });
+	};
+	const handelGreen = () => {
+		setState({ red: "noactive", yellow: "noactive", green: "active" });
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div id="trafficTop" />
+			<div id="container">
+				<div className="red" id={state.red} onClick={handelRed} />
+				<div
+					className="yellow"
+					id={state.yellow}
+					onClick={handelYellow}
+				/>
+				<div className="green" id={state.green} onClick={handelGreen} />
+			</div>
 		</div>
 	);
 }
+export default Home;
